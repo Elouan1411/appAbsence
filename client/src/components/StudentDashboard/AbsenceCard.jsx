01c39fa383b13b2e06278213c0768c55ea6dd539
Elouan1411 <boiteux.elouan@gmail.com>
2026-01-12 10:14:16 +0100

    start dashboard student

---
+++ client/src/components/StudentDashboard/AbsenceCard.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import "../../style/Student.css";
+import toast from "react-hot-toast";
+
+const AbsenceCard = ({ subject, date, startTime, endTime, status, onAction }) => {
+    return <div></div>;
+};
+
+export default AbsenceCard;
