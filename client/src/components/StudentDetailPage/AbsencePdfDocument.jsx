fcde3d3b5a11ff0638bccbb49bdbc2c05f84eb10
Aymeric <aymeric.mariaux@gmail.com>
2026-01-20 18:10:23 +0100

    correction generation pdf multi page + ajout informations

--- client/src/components/StudentDetailPage/AbsencePdfDocument.jsx
+++ client/src/components/StudentDetailPage/AbsencePdfDocument.jsx
@@ -14,12 +14,18 @@ const styles = StyleSheet.create({
         borderBottomColor: "#112131", 
         paddingBottom: 10 
     },
+    TitleContainer: {
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        marginBottom: 15,
+    },
     title: { 
         fontSize: 24, 
         textAlign: "center", 
         fontWeight: "bold",
         justifyContent: "center",
-        marginBottom: 15, 
+        marginBottom: 5, 
         color: "#112131" 
     },
     infoContainer: { 
@@ -64,29 +70,53 @@ const styles = StyleSheet.create({
 });
 
 const AbsencePdfDocument = ({ student, absences }) => {
-    const currentYear = new Date().getFullYear();
-    const absenceList = Object.values(absences);
+    const date = new Date();
+    const currentYear = date.getFullYear();
+    const month = date.getMonth();
+    const startingYear = month < 8 ? currentYear - 1 : currentYear;
+    const endingYear = month < 8 ? currentYear : currentYear + 1;
+    const absenceList = Object.values(absences).sort(
+        (a, b) => new Date(b.debut) - new Date(a.debut)
+    );
 
     return (
         <Document>
             <Page size="A4" style={styles.page}>
                 <View style={styles.header}>
-                    <Text style={styles.title}>Relevé d'absences {currentYear}</Text>
+                    <View style={styles.TitleContainer}>
+                        <Text style={styles.title}>Relevé d'absences {startingYear} / {endingYear}</Text>
+                        <Text style={{ fontSize: 15 }}>du 1 septembre {startingYear} au {new Date().toLocaleDateString("fr-FR", { day: "numeric", month: "long", year: "numeric" })}</Text>
+                    </View>
 
+                    <View>
                     <View style={styles.infoContainer}>
                         <View>
-                            <Text>Nom : {student.nom} {student.prenom}</Text>
-                            <Text>Promo : {student.promo}</Text>
+                            <Text>Nom : {student.nom}</Text>
+                            <Text>Prénom : {student.prenom}</Text>
                         </View>
                         <View style={{ alignItems: "flex-end" }}>
                             <Text>N° Étudiant : {student.numeroEtudiant}</Text>
                             <Text>Login ENT : {student.loginENT}</Text>
                         </View>
+                        </View>
+
+                        <View style={{ marginTop: 10 }}>
+                            <View style={{ flexDirection: "row", justifyContent: "space-between", marginBottom: 5 }}>
+                                <Text>Promo impair: {student.promo}</Text>
+                                <Text> TD impair : {student.groupeTD}</Text>
+                                <Text> TP impair : {student.groupeTP}</Text>
+                            </View>
+                            <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+                                <Text>Promo pair: {student.promoPair}</Text>
+                                <Text> TD pair : {student.groupeTDPair}</Text>
+                                <Text> TP pair : {student.groupeTPPair}</Text>
+                            </View>
                     </View>
+                </View>
                 </View>
 
                 <View style={styles.table}>
-                    <View style={styles.row}>
+                    <View style={styles.row} fixed>
                         <View style={styles.col}>
                             <Text style={[styles.cell, styles.headerCell]}>Date & Heure de début</Text>
                         </View>
@@ -129,7 +159,11 @@ const AbsencePdfDocument = ({ student, absences }) => {
                     ))}
                 </View>
 
-                <Text style={styles.footer}>Généré le {new Date().toLocaleDateString("fr-FR")}</Text>
+                <Text style={styles.footer}>
+                    Généré le {new Date().toLocaleDateString("fr-FR")}
+                    {"\n"}
+                    Nombre total d'absences : {absenceList.length}
+                </Text>
             </Page>
         </Document>
     );
