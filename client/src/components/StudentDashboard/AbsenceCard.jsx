fe43ca3825163f4b6ca581929a3995a8623cc8ad
Elouan1411 <boiteux.elouan@gmail.com>
2026-01-21 12:40:56 +0100

    dashboard etudiant : Eye pas dans todo

--- client/src/components/StudentDashboard/AbsenceCard.jsx
+++ client/src/components/StudentDashboard/AbsenceCard.jsx
@@ -111,7 +111,7 @@ const AbsenceCard = ({
 
             <div className="card-absence-right">
                 <div className={`action-button-wrapper ${isSelectionMode ? "hidden" : ""}`}>
-                    <Eye className="icon-eye details-icon" onClick={handleDetails} />
+                    {status !== "todo" && <Eye className="icon-eye details-icon" onClick={handleDetails} />}
                     {status === "todo" && (
                         <button className="btn-justifier" onClick={adminComment ? handleDetails : handleJustify}>
                             {adminComment ? "Modifier" : "Justifier"}
