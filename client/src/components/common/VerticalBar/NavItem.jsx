73a527eeee1ae558f2e74ccc61faf03f407de111
Elouan1411 <boiteux.elouan@gmail.com>
2026-01-24 14:47:32 +0100

    reload si meme destination + correction API_URL

--- client/src/components/common/VerticalBar/NavItem.jsx
+++ client/src/components/common/VerticalBar/NavItem.jsx
@@ -3,10 +3,12 @@ import { NavLink } from "react-router-dom";
 import getIconClass from "../../../functions/getIconClass";
 import { useSafeNavigate } from "../../../hooks/useSafeNavigate";
 import { useUnsaved } from "../../../context/UnsavedContext";
+import { useLocation } from "react-router-dom";
 
 function NavItem({ link, index, to, isMenuOpen }) {
     const { hasUnsavedChanges } = useUnsaved();
     const safeNavigate = useSafeNavigate(hasUnsavedChanges);
+    const location = useLocation();
     const iconName = link.icon || getIconClass(link.label);
     return (
         <li key={index} className="nav-item">
@@ -16,6 +18,11 @@ function NavItem({ link, index, to, isMenuOpen }) {
                 className={({ isActive }) => (isActive ? "nav-link active" : "nav-link")}
                 onClick={(e) => {
                     e.preventDefault();
+                    // reload if it's the same page
+                    if (location.pathname === to) {
+                        window.location.reload();
+                        return;
+                    }
                     safeNavigate(to);
                 }}
             >
