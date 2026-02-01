77335c5b5fc4b75b8687a2bdb72df9a88d9b9824
Killian Mathias <killianmathias@icloud.com>
2026-02-01 16:19:05 +0100

    Ajout d'une petite animation d'apparition de page

--- client/src/components/Layouts/Layout.jsx
+++ client/src/components/Layouts/Layout.jsx
@@ -1,15 +1,29 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import VerticalBar from "../common/VerticalBar/VerticalBar";
 import "../../style/Layout.css";
+import { AnimatePresence, motion } from "framer-motion";
 
 const Layout = () => {
+    const location = useLocation();
+
     return (
         <div className="layout">
             <VerticalBar />
 
             <main>
-                <Outlet />
+                <AnimatePresence mode="wait">
+                    <motion.div
+                        key={location.pathname}
+                        initial={{ opacity: 0, y: 10 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        exit={{ opacity: 0 }}
+                        transition={{ duration: 0.15, ease: "easeOut" }}
+                        style={{ width: "100%", height: "100%", display: "flex", flexDirection: "column" }}
+                    >
+                        <Outlet />
+                    </motion.div>
+                </AnimatePresence>
             </main>
         </div>
     );
