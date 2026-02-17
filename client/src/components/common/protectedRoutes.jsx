cb1feab4b961a36655e3a648d4c6fbd4f743c82b
Aymeric <aymeric.mariaux@gmail.com>
2026-02-17 17:17:08 +0100

    correction acces pages

--- client/src/components/common/protectedRoutes.jsx
+++ client/src/components/common/protectedRoutes.jsx
@@ -1,16 +1,17 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth";
 import Layout from "../Layouts/Layout";
+import { getHomeRoute } from "../../utils/roleUtils";
 
-export function ProtectedRoutes({ roles }) {
+export function ProtectedRoutes({ allowedRoles }) {
     const { user, role } = useAuth();
 
     if (!user) {
         return <Navigate to="/" replace />;
     }
 
-    if (roles && !roles.includes(role)) {
-        return <Navigate to="/error" replace />;
+    if (allowedRoles && !allowedRoles.includes(role)) {
+        return <Navigate to={getHomeRoute(role)} replace />;
     }
 
     return <Layout />;
